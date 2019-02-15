2a2ad7802aa464d2db74812cede3098d4f1cf363
Brian Keaveney <38507940+BrianKeaveney@users.noreply.github.com>
2019-02-15 23:46:36 +0000

    almost fin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,12 +18,14 @@ export class AppComponent {
       '/assets/images/products/black-hat.jpg',
       ['Men', 'Accessories', 'Hats'],
       29.99),
-      new Product('MYSHOES',
+      new Product(
+      'MYSHOES',
       'Black Running Shoes',
       '/assets/images/products/black-shoes.jpg',
       ['Men', 'Shoes', 'Running Shoes'],
       109.99),
-      new Product('NEATOJACKET',
+      new Product(
+      'NEATOJACKET',
       'Blue Jacket',
       '/assets/images/products/blue-jacket.jpg',
       ['Women', 'Apparel', 'Jackets & Vests'],
